bf16f12f365c3f1f88ac0f6456842f62cd5c11dc
Mu-Lambda <mulambda@mulambda.de>
2025-01-15 16:29:40 +0100

    Intial motion

---
+++ motion.c
@@ -0,0 +1,6 @@
+#ifndef __MOTION_H
+#define __MOTION_H
+
+int is_motion(char *portname);
+
+#endif
---
+++ motion_main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "motion.h"
+
+int main(int argc, char *argv[]) {
+    is_motion(argv[1]);
+}
